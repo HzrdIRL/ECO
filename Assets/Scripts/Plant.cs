a491baa1c7dbfef97bd6c1c309c863743c49d7b2
HzrdIRL <hzrdtv@gmail.com>
2017-05-15 16:16:39 +1000

    Added functionality for interacting with objects, Interactable interface for abstraction

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -1,21 +1,61 @@
 using UnityEngine;
 
-public abstract class Plant : MonoBehaviour {
-		public int age;
-		public bool fruiting;
-		public bool wilted;
-		public int daysToFruit = 1;
-    public int daysSinceHarvest = 0;
+public abstract class Plant : MonoBehaviour, Interactable {
+    public int age;
+    public bool fruiting;
+    public bool wilted;
+    public int daysToFruit;
+    public float value;
+    public int daysSinceHarvest;
     public SpriteRenderer sprite;
+    public bool watered;
     //public Sprite[] images = new Sprite[4];
 
     public int[] stageLengths = {0,1,2};
 
-    public void ageUp(bool watered)
+	// Use this for initialization
+	void Start () {
+		wilted = false;
+        watered = false;
+		fruiting = false;
+        age = 0;
+        daysSinceHarvest = 0;
+        
+    }
+
+	public void kill() {
+		Destroy(this.gameObject);
+	}
+
+	void Update() {
+
+	}
+
+    void Interactable.interact()
     {
+        if (fruiting)
+        {
+            PlayerController.bioMatter += value;
+            daysSinceHarvest = 0;
+            fruiting = false;
+        }
+        else
+        {
+            Debug.Log("Days to fruiting: " + ((int)Stages.Matured - age));
+        }
+    }
+
+    /*
+     * Handle the aging of the flower at the end of a day cycle
+     */
+    public void ageUp()
+    {
+        sprite = gameObject.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
         if (watered)
         {
             age++;
+            wilted = false;
+            watered = false;
         }
         else if (wilted)
         {
@@ -26,44 +66,37 @@ public abstract class Plant : MonoBehaviour {
             wilted = true;
         }
 
-        if (age >= stageLengths[(int)Stages.Matured])
+        if (!wilted)
         {
-            sprite = gameObject.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
-            sprite.color = Color.blue;
-						if (fruiting)
+            if (age >= stageLengths[(int)Stages.Matured])
             {
-                sprite.color = Color.red;
-                daysSinceHarvest = 0;
+                if (fruiting)
+                {
+                    sprite.color = Color.green;
+                }
+                else if (daysSinceHarvest >= daysToFruit)
+                {
+                    sprite.color = Color.blue;
+                    fruiting = true;
+                }
             }
-        }
-        else if (age >= stageLengths[(int)Stages.Sprouted])
-        {
-            if (!wilted)
+            else if (age >= stageLengths[(int)Stages.Sprouted])
             {
                 if (daysSinceHarvest == daysToFruit)
                 {
-                    fruiting = true;
                     sprite.color = Color.green;
                 }
-            } else {
-							sprite.color = Color.black;
-						}
+            }
+            else
+            {
+                sprite.color = Color.blue;
+            }
         }
+        else
+        {
+            sprite.color = Color.black;
+        }
+
+        daysSinceHarvest++;
     }
-
-
-		// Use this for initialization
-		void Start () {
-			wilted = false;
-			fruiting = false;
-      age = 0;
-		}
-
-		void kill() {
-			Destroy(gameObject);
-		}
-
-		void Update() {
-
-		}
 }
